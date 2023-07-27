11cd078d453a316a32f3a973e7771805c2063d98
BROOKXAT <108485252+BROOKXAT@users.noreply.github.com>
2023-07-27 16:21:53 +0100

    done

--- quick_sort.py
+++ quick_sort.py
@@ -1,19 +1,33 @@
-def swap(l) :
-    pivot = l[-1]
-    swap_marker = -1
-    for  i,j in enumerate(l) :
+def swap(l,start, end) :
+    '''
+    this function take the last element (pivot) and place it at the right index
+    every element at the right of pivot is lesser then him 
+    and the elements at the left of pivot are greather then him 
+    and return the index of the pivot
+    '''
+    # pivot is the last element 
+    pivot = l[end]
+    swap_marker = start -1
+    for  i in range(start,end+1) :
         if l[i] > pivot :
-            print("curent: ",i," swap marker: ",swap_marker)
+
             continue
         else :
             swap_marker += 1
             if swap_marker == i :
-                print("curent: ",i," swap marker: ",swap_marker)
+    
                 continue
             if swap_marker < i :
                 l[i] , l[swap_marker] = l[swap_marker] , l[i]
-                print("curent: ",i," swap marker: ",swap_marker)
-    return l
-        
+    return swap_marker
+
+def quick_sort(l,start,end) :
+    if start < end :
+        pivot_index = swap(l,start,end)
+        quick_sort(l,start,pivot_index-1)
+        quick_sort(l,pivot_index+1,end)
+
+
 l = [3,2,0,1,5,8,7,6,9,4]
-print(swap(l))
+print(quick_sort(l,0,9))
+print(l)
